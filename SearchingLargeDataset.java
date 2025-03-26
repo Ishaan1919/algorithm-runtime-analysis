9253daca905713811945efb365c0c3ac73a6683a
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-26 14:46:05 +0530

    Program to check the time differnce for finding the element using linear and binary search

---
+++ SearchingLargeDataset.java
@@ -0,0 +1,59 @@
+public class SearchingLargeDataset {
+    public static void main(String[] args) {
+        int size = 1000000;
+        int[] arr = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i] = i+1;
+        }
+        int target = size;
+
+        long startTime = System.nanoTime();
+        linearSearch(arr,target);
+        long endTime = System.nanoTime();
+
+        System.out.println("Time take by linear search: " + (endTime-startTime));
+
+        startTime = System.nanoTime();
+        binarySearch(arr,target);
+        endTime = System.nanoTime();
+        System.out.println("Time take by binary search: " + (endTime-startTime));
+
+//        Array size = 1000
+//        Time take by linear search: 32701
+//        Time take by binary search: 6400
+
+//        Array size = 10000
+//        Time take by linear search: 145300
+//        Time take by binary search: 7100
+
+//        Array size = 100000
+//        Time take by linear search: 5430500
+//        Time take by binary search: 11100
+
+    }
+
+    public static boolean linearSearch(int[] arr, int target){
+        for(int i:arr){
+            if(i==target) return true;
+        }
+        return false;
+    }
+
+    public static boolean binarySearch(int[] arr, int target){
+        int l = 0;
+        int h = arr.length-1;
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(arr[mid]==target){
+                return true;
+            }
+            else if(arr[mid]>target){
+                h = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return true;
+    }
+}
